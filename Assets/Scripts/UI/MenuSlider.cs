c2a1cba7549e61d72e3eadd1155ebce825fa3f83
Oliver Dahlqvist <Oliver_HHC@hotmail.com>
2017-12-05 14:17:09 +0100

    Added level select menu

---
+++ Assets/Scripts/UI/MenuSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSlider : MonoBehaviour {
+    Slider slider;
+    Text sliderText;
+
+	void Start () {
+        slider = GetComponent<Slider>();
+        sliderText = GetComponentInChildren<Text>();
+
+        SetText(slider.value.ToString());
+    }
+	
+	void Update () {
+		
+	}
+
+    public void OnValueChange()
+    {
+        SetText(slider.value.ToString());
+    }
+    private void SetText(string text)
+    {
+        sliderText.text = text;
+    }
+}
