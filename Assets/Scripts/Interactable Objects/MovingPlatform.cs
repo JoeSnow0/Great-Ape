d1a7d27aa38cad2f4271f4f1f9c04cd5eed81c02
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-12-14 13:05:19 +0100

    added new body parts to animation

---
+++ Assets/Scripts/Interactable Objects/MovingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Vector2 velocity;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        rb2d.velocity = velocity;
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        velocity.x *= -1;
+    }
+}
