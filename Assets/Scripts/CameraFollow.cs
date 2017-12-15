473843649c66114a617d0e721346bbd0b3365994
Oskar Svensson <oskar_erik@hotmail.com>
2017-12-15 13:03:31 +0100

    - The camera lerps between apes when you switch between them - You can switch between apes using TAB or L.Shift+TAB

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,14 +5,15 @@ public class CameraFollow : MonoBehaviour
 {
     public ManagerConfig managerConfig;
     public Vector3 arrowOffset;
-    // Update is called once per frame
+    public float cameraLerpScale = 1f;
+
     void Update()
     {
         if(ApeSelectionController.activeApe != null)
         {
             if (!(transform.position == ApeSelectionController.activeApe.transform.position + arrowOffset))
             {
-                transform.position = ApeSelectionController.activeApe.transform.position + arrowOffset;
+                transform.position = Vector3.Lerp(transform.position, ApeSelectionController.activeApe.transform.position + arrowOffset, cameraLerpScale);
             }
         }
     }
