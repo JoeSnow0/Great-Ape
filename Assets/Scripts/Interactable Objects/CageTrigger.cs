0f6b767c2944021674c3bec0d1d41e3b80eb6164
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-01-09 11:25:07 +0100

    fixed variable names in cageTrigger script.

--- Assets/Scripts/Interactable Objects/CageTrigger.cs
+++ Assets/Scripts/Interactable Objects/CageTrigger.cs
@@ -3,30 +3,30 @@ using System.Collections;
 
 public class CageTrigger : MonoBehaviour
 {
-    [SerializeField] ManagerConfig managerConfig;
+    [SerializeField] private ManagerConfig m_managerConfig;
     [Header("These should always be the same type of ape")]
-    [SerializeField] GameObject apeModel;
-    [SerializeField] Player apeToSpawn;
+    [SerializeField] private GameObject m_apeModel;
+    [SerializeField] private Player m_apeToSpawn;
     [Header("SpawnPoint")]
-    [SerializeField] Transform modelTransform;
-    private BoxCollider2D collider2d;
+    [SerializeField] private Transform m_modelTransform;
+    private BoxCollider2D m_collider2d;
 
     
     void Start()
     {
-        managerConfig = FindObjectOfType<ManagerConfig>();
-        collider2d = GetComponent<BoxCollider2D>();
+        m_managerConfig = FindObjectOfType<ManagerConfig>();
+        m_collider2d = GetComponent<BoxCollider2D>();
         //add apeModel to cage
-        Instantiate(apeModel, modelTransform);
+        Instantiate(m_apeModel, m_modelTransform);
     }
 
-    void DestroyCage()
+    void TriggerCage()
     {
         //Spawn Ape
-        managerConfig.apeSelectionController.AddApe(apeToSpawn, modelTransform.position, modelTransform.rotation);
-        modelTransform.gameObject.SetActive(false);
+        m_managerConfig.apeSelectionController.AddApe(m_apeToSpawn, m_modelTransform.position, m_modelTransform.rotation);
+        m_modelTransform.gameObject.SetActive(false);
         //Disable cage
-        collider2d.enabled = false;
+        m_collider2d.enabled = false;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -34,7 +34,7 @@ public class CageTrigger : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.F))
             {
-                DestroyCage();
+                TriggerCage();
             }
         }
     }
