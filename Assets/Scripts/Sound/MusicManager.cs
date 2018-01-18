9849a074fda009cd719f0561a308ad28dad073d3
JoeSnow0 <thomas.c.richards1@gmail.com>
2018-01-18 12:09:41 +0100

    added sound effects to completing a level. added sound and music managers.

---
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    [System.NonSerialized]
+    public static AudioSource m_Source;
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        m_Source = GetComponent<AudioSource>();
+        UpdateVolume();
+    }
+
+    public static void UpdateVolume()
+    {
+        if (m_Source.volume <= 0)
+        {
+            m_Source.mute = true;
+        }
+        else
+            m_Source.mute = false;
+    }
+
+    public static void PlaySound(AudioClip clip, float volMod = 1)
+    {
+        m_Source.clip = clip;
+        m_Source.volume = m_Source.volume * volMod;
+        m_Source.Play();
+    }
+
+    public static void Stop()
+    {
+        m_Source.Stop();
+    }
+}
---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    private static AudioSource m_Source;
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        m_Source = GetComponent<AudioSource>();
+        UpdateVolume();
+    }
+
+    public static void UpdateVolume()
+    {
+        if (m_Source.volume <= 0)
+        {
+            m_Source.mute = true;
+        }
+        else
+            m_Source.mute = false;
+    }
+
+    public static void PlaySound(AudioClip clip, float volMod = 1)
+    {
+        m_Source.pitch = 1;
+        m_Source.PlayOneShot(clip, m_Source.volume * volMod);
+    }
+    public static void PlayPitched(AudioClip clip, float minPitch, float maxPitch, float volMod = 1)
+    {
+        m_Source.pitch = Random.Range(minPitch, maxPitch);
+        m_Source.PlayOneShot(clip, m_Source.volume * volMod);
+    }
+    public static void PlayDelayed(AudioClip clip, float delay)
+    {
+        m_Source.clip = clip;
+        m_Source.PlayDelayed(delay);
+    }
+}
